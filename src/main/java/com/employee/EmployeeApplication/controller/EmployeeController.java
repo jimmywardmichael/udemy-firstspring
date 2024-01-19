a1efc24a10f6419f8f6d87712a8aeb753324e409
James Ward <jimmywardmichael@gmail.com>
2024-01-18 18:08:11 -0600

    added update and delete employee

--- src/main/java/com/employee/EmployeeApplication/controller/EmployeeController.java
+++ src/main/java/com/employee/EmployeeApplication/controller/EmployeeController.java
@@ -3,9 +3,7 @@ package com.employee.EmployeeApplication.controller;
 import com.employee.EmployeeApplication.entity.Employee;
 import com.employee.EmployeeApplication.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +24,22 @@ public class EmployeeController {
     @RequestMapping("/employees/{id}")
     public Employee findAnEmployee(@PathVariable int id) {
         return employeeService.getAnEmployee(id);
-
         }
+        //create a new employee using post
+@RequestMapping(value = "/employees", method = RequestMethod.POST)
+    public  void createEmployee(@RequestBody Employee employee){
+        employeeService.createEmployee(employee);
+    }
+//updates employee records
+    @RequestMapping(value = "/employees/{id}", method = RequestMethod.PUT)
+    public void updateEmployee(@PathVariable int id,@RequestBody Employee employee){
+        employeeService.updateEmployee(employee);
+    }
+    //delete employee
+    @RequestMapping(value = "/employees/{id}", method = RequestMethod.DELETE)
+    public List<Employee> deleteEmployee(@PathVariable int id){
+        employeeService.deleteEmployee(id);
+        return employeeService.getAllEmployees();
+    }
     }
 
--- src/main/java/com/employee/EmployeeApplication/service/EmployeeService.java
+++ src/main/java/com/employee/EmployeeApplication/service/EmployeeService.java
@@ -4,15 +4,16 @@ package com.employee.EmployeeApplication.service;
 import com.employee.EmployeeApplication.entity.Employee;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class EmployeeService {
-       List<Employee> employeeList = Arrays.asList(
+       List<Employee> employeeList = new ArrayList<>(Arrays.asList(
                 new Employee(1, "First Employee", "Washington"),
                 new Employee(2, "Second Employee", "New York")
-        );
+        ));
        public List<Employee> getAllEmployees(){
         return employeeList;
     }
@@ -20,5 +21,31 @@ public class EmployeeService {
            return employeeList.stream().filter(e -> (
                    e.getEmployeeId() == id)).findFirst().get();
     }
+//Getting the employee object from the class
+    public void createEmployee(Employee employee){
+           employeeList.add(employee);
+    }
+    //update employee record
+    public void updateEmployee(Employee employee){
+          List<Employee> tempEmployee = new ArrayList<>();
+          for (Employee emp : employeeList){
+              if (emp.getEmployeeId() == employee.getEmployeeId()){
+                  emp.setEmployeeName(employee.getEmployeeName());
+                  emp.setEmployeeCity(employee.getEmployeeCity());
+              }
+              tempEmployee.add(emp);
+          }
+          this.employeeList = tempEmployee;
+    }
 
+    //delete an employee
+    public void deleteEmployee(int id) {
+           ArrayList<Employee> tempEmployee = new ArrayList<>();
+           for (Employee emp : employeeList){
+               if(emp.getEmployeeId() == id)
+                   continue;
+               tempEmployee.add(emp);
+           }
+           this.employeeList = tempEmployee;
+    }
 }
